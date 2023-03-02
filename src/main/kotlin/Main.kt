2e36f017ada621f00d9f4a7f58db1122ca0a0961
Natasha O <photographie@yandex.ru>
2023-03-02 16:18:33 +0300

    Commit2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,15 @@
 fun main() {
-    val likes: Int = 25;
-    var result: String = "людей";
+    val likes: Int = 21;
+    var result: String = "люди";
 
-    if (likes % 10 == 1 || likes % 100 == 12 || likes % 100 == 13 || likes % 100 == 14) {
-            result = "человек"
-    }
-      else if  (likes % 10 == 2 || likes % 10 == 3 || likes % 10 == 4) {
-            result = "человека"
+    if (likes % 10 == 1 && likes % 100 !== 11) {
+            result = "человеку"
     }
     else {
-        result = "людей"
+        result = "людям"
 
     }
-    println(result);
+    println("Понравилось " + likes + " " + result);
 
 }
 
